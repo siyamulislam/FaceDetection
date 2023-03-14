6ebb27d1c968989aed3c0a68fbefe6c2fe7c91de
siyamulislam <siyamulislam1@gmail.com>
2023-03-14 23:01:05 +0600

    full face detection with custom rectangle

--- FaceDetectionModule.py
+++ FaceDetectionModule.py
@@ -6,12 +6,11 @@ import time
 # def __init__
 
 class FaceDetector():
-    def __init__(self, minDetectionCon=.9):
+    def __init__(self, minDetectionCon=0.9):
         self.minDetectionCon = minDetectionCon
-
         self.mpDraw = mp.solutions.drawing_utils
         self.mpFaceDetection = mp.solutions.face_detection
-        self.faceDetection = self.mpFaceDetection.FaceDetection(minDetectionCon)
+        self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)
 
     def findFaces(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -28,17 +27,37 @@ class FaceDetector():
                                                ih), int(bboxC.width*iw), int(bboxC.height*ih)
                 bboxs.append([id, bbox, detection.score])
                 # print(detection)
-                cv2.rectangle(img, bbox, (0, 255, 0), 2)
+                # cv2.rectangle(img, bbox, (0, 255, 0), 2)
+                if draw:
+                    img=self.fancyDraw(img,bbox)
+
                 cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0], bbox[1]-15),
-                            cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+                            cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
         return img, bboxs
 
+    def fancyDraw(self,img,bbox,l=15,t=2,rt=1):
+        x,y,w,h=bbox
+        x1,y1=x+w, y+h
+        cv2.rectangle(img, bbox, (0, 255, 0), rt)
+        # topLeft
+        cv2.line(img,(x,y),(x+l,y),(0, 255, 0),t)
+        cv2.line(img,(x,y),(x,y+l),(0, 255, 0),t)
+        # topRight
+        cv2.line(img,(x1,y),(x1-l,y),(0, 255, 0),t)
+        cv2.line(img,(x1,y),(x1,y+l),(0, 255, 0),t)
+        # bottomLeft
+        cv2.line(img,(x,y1),(x+l,y1),(0, 255, 0),t)
+        cv2.line(img,(x,y1),(x,y1-l),(0, 255, 0),t)
+        # bottomRight
+        cv2.line(img,(x1,y1),(x1-l,y1),(0, 255, 0),t)
+        cv2.line(img,(x1,y1),(x1,y1-l),(0, 255, 0),t)
+        return img
 
 def main():
     pTime = 0;cTime = 0
     # cap = cv2.VideoCapture('video/1.mp4')
     cap = cv2.VideoCapture(0)
-    detector = FaceDetector(minDetectionCon=0.9)
+    detector = FaceDetector(0.75)
     if (cap.isOpened() == False): 
         print("Unable to read camera feed")
 
@@ -48,7 +67,7 @@ def main():
             print("Can't receive img (stream end?). Exiting ...")
             break
         img, bboxs=detector.findFaces(img)
-        print(bboxs)
+        # print(bboxs)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
